eb6bff1f6f11ab0cf283ba926783d381ce5a88d5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-19 09:35:19 -0500

    :sparkles: Update Calc

--- src/calc/main.go
+++ src/calc/main.go
@@ -5,13 +5,18 @@ import "fmt"
 func main() {
 	var operator string
 	var number1, number2 int
+
 	fmt.Print("Please enter First number: ")
 	fmt.Scanln(&number1)
+
 	fmt.Print("Please enter Second number: ")
 	fmt.Scanln(&number2)
+
 	fmt.Print("Please enter Operator (+,-,/,%,*):")
 	fmt.Scanln(&operator)
+
 	output := 0
+
 	switch operator {
 	case "+":
 		output = number1 + number2
@@ -26,5 +31,6 @@ func main() {
 	default:
 		fmt.Println("Invalid Operation.")
 	}
-	fmt.Printf("%d %s %d = %d", number1, operator, number2, output)
+
+	fmt.Printf("%d %s %d = %d\n", number1, operator, number2, output)
 }
