a27b31a43f5d7117494c128142df6ea4e47b899a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-19 08:49:35 -0500

    :sparkles: Add Tasklist

---
+++ src/tasks/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type task struct {
+	name        string
+	description string
+	completed   bool
+}
+
+func createTask(name string, description string, completed bool) task {
+	return task{
+		name:        name,
+		description: description,
+		completed:   completed,
+	}
+}
+
+func main() {
+	myTask := createTask("End my Go course", "End mi Golang course on Platzi today.", false)
+
+	fmt.Printf("%+v\n", myTask)
+}
