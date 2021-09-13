0d81eb518d84838604b58a071fcc3ceb110e6f85
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-13 10:01:16 -0500

    :sparkles: Update While Go

--- src/while/main.go
+++ src/while/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	// For Instead of While | NICE :)
-	for true {
+        // It can be `for (condition)`
+        // Or just for is a while true
+	for {
 		fmt.Println("Hi!")
 	}
 }
